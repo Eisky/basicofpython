c7a42ebb056cc2492d9dd9624109ab24d03ddf12
sunki <417584301@qq.com>
2017-07-25 18:37:32 +0800

    new

--- string_format.py
+++ string_format.py
@@ -4,3 +4,8 @@ age = input('age:').strip()
 job = input('job:').strip()
 print('Information of %s:\n'
       ' \nName:%s\nAge:%s\nJob:%s' % (name, name, age, job))
+s1 = '{name} is {actor}'
+d = {'name':'alex', 'actor':'good boy'}
+
+result = s1.format(**d)
+print(result)
