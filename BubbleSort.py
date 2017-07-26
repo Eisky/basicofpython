b4e79c7035805f4c2c4280013900a4fff465df84
sunki <417584301@qq.com>
2017-07-26 13:48:43 +0800

    BubbleSort

---
+++ BubbleSort.py
@@ -0,0 +1,10 @@
+data = [10, 4, 33, 21, 54, 3, 8, 11, 5, 22, 2, 1, 17, 13, 6]
+
+for j in range(len(data)):
+    for i in range(len(data) - 1):
+        if data[i] > data[i + 1] :
+            tmp = data[i]
+            data[i]=data[i+1]
+            data[i+1]=tmp
+
+print(data)
